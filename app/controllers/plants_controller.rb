e7b71e1a3d7e4261faa6ab72119d66cd25e8bfe3
Sam M <sammontalvojr@gmail.com>
2020-07-24 01:31:55 -0500

    navigation buttons to indexes, forms for new plants and users

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,35 @@
 class PlantsController < ApplicationController
+    before_action :set_plant, only: [:show, :edit]
     def index
         @plants = Plant.all
+    end
+
+    def show
+    end
+
+    def new
+        @plant = Plant.new
+    end
+
+    def create
+        @plant = Plant.new(plant_params)
+        if @plant.valid?
+            @plant.save
+            redirect_to plant_path(@plant)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    private
+    def set_plant
+        @plant = Plant.find(params[:id])
+    end
+
+    def plant_params
+        params.require(:plant).permit(:name, :species, :watering_gap, :optimal_humidity)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,27 @@ class UsersController < ApplicationController
     end
 
     def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def edit
     end
 
     private
+    def user_params
+        params.require(:user).permit(:name, :occupation, :age)
+    end
+
     def set_user
         @user = User.find(params[:id])
     end
