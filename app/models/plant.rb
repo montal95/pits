1b814df320033764e814a24f95a5ec6e96bb9af9
Sam M <sammontalvojr@gmail.com>
2020-07-23 23:07:07 -0500

    finished model relationships and validation

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,8 @@
 class Plant < ApplicationRecord
+    has_many :greenhouses
+    has_many :users, through: :greenhouses
+    validates :name, presence: true, uniqueness: true, length: {:minimum => 4}
+    validates :species, presence: true, length: {:minimum => 4}
+    validates :watering_gap, numericality: { only_integer: true }, presence: true
+    validates :optimal_humidity, numericality: { only_integer: true }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :greenhouses
+    has_many :plants, through: :greenhouses
+    validates :name, presence: true, uniqueness: true, length: {:minimum => 4}
+    validates :occupation, presence: true
+    validates :age, numericality: { only_integer: true }, presence: true
 end
